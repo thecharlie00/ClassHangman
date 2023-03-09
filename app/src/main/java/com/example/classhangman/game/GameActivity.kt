ce2c1c535e7190c8e83f3ee6c771a116994bfcde
Pau Garcia Gozalvez <pgarcia@ledmotive.com>
2023-03-09 17:14:24 +0100

    MVVM applied to hangman

--- app/src/main/java/com/example/classhangman/game/GameActivity.kt
+++ app/src/main/java/com/example/classhangman/game/GameActivity.kt
@@ -1,16 +1,17 @@
 package com.example.classhangman.game
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
 import com.example.classhangman.databinding.ActivityGameBinding
 import com.example.classhangman.ranking.RankingActivity
 
 class GameActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityGameBinding
-    lateinit var hangmanModelView: HangmanModelView
+    val hangmanModelView by viewModels<HangmanModelView>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,15 +19,33 @@ class GameActivity : AppCompatActivity() {
         binding = ActivityGameBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        hangmanModelView = HangmanModelView()
-        hangmanModelView.getNewWord(binding.hagmanTextOuput)
+
+        hangmanModelView.hangman.observe(this) {
+            binding.hagmanTextOuput.text = it.word.replace("_", "_ ")
+
+            if (it?.correct == false)
+                Toast.makeText(this, "This letter is not in the word!", Toast.LENGTH_SHORT).show()
+        }
+
+        hangmanModelView.alphabet.observe(this) {
+            var usedLetters = ""
+            it.forEach { (char, isUsed) ->
+                if (isUsed)
+                    usedLetters += "$char, "
+            }
+            binding.alphabet.text = usedLetters
+        }
+
+        hangmanModelView.getNewWord()
 
 
         binding.guessButton.setOnClickListener {
-            val char = binding.guessLetterInput.text.getOrNull(0)
-            if (char != null)
-                hangmanModelView.guessLetter(char, binding.hagmanTextOuput, binding.alphabet, this)
-            else
+            val letter = binding.guessLetterInput.text.getOrNull(0)
+
+            if (letter != null) {
+                if (!hangmanModelView.guessLetter(letter))
+                    Toast.makeText(this, "You already used letter $letter", Toast.LENGTH_SHORT).show()
+            } else
                 Toast.makeText(this, "You must submit a letter", Toast.LENGTH_SHORT).show()
 
             binding.guessLetterInput.setText("")
--- app/src/main/java/com/example/classhangman/game/HangmanModelView.kt
+++ app/src/main/java/com/example/classhangman/game/HangmanModelView.kt
@@ -1,98 +1,72 @@
 package com.example.classhangman.game
 
-import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class HangmanModelView {
+class HangmanModelView : ViewModel() {
 
-    val username = "gg class"
-    var hangman: HangmanModel? = null
-    val outside = Retrofit.Builder()
+    var hangman = MutableLiveData<HangmanModel>()
+
+    val alphabet = MutableLiveData<HashMap<Char, Boolean>>().also { alphaCreation ->
+        hashMapOf<Char, Boolean>().also {
+            ('a'..'z').forEach { c ->
+                it[c] = false
+            }
+            alphaCreation.postValue(it)
+        }
+    }
+
+    private val outside = Retrofit.Builder()
         .baseUrl("http://hangman.enti.cat:5002")
         .addConverterFactory(GsonConverterFactory.create())
         .build()
 
-    val alphabet = hashMapOf<Char, Boolean>().also {
-        ('a'..'z').forEach { c ->
-            it[c] = false
-        }
-    }
-
-    fun getNewWord(hagmanTextOuput: TextView) {
+    fun getNewWord() {
         val request = outside.create(ApiHangman::class.java)
         request.getNewWord("es").enqueue(object : Callback<HangmanModel> {
             override fun onResponse(call: Call<HangmanModel>, response: Response<HangmanModel>) {
-                hangman = response.body() ?: return
-                hagmanTextOuput.text = hangman?.word?.replace("_", "_ ") ?: ""
+                hangman.postValue(response.body() ?: return)
             }
 
             override fun onFailure(call: Call<HangmanModel>, t: Throwable) {
-                hagmanTextOuput.text = "Error: $t"
             }
         })
     }
 
-    fun guessLetter(
-        letter: Char,
-        hagmanTextOuput: TextView,
-        alphabetTextView: TextView,
-        context: GameActivity
-    ) {
+    fun guessLetter(letter: Char): Boolean {
         val request = outside.create(ApiHangman::class.java)
-        val formattedLetter = letter.lowercase()
+        val formattedLetter = letter.lowercase()[0]
 
-        if (alphabet[formattedLetter[0]] == true) {
-            Toast.makeText(context, "You already used letter $letter", Toast.LENGTH_SHORT).show()
-            return
-        }
+        // Verify if letter is already checked
+        if (alphabet.value?.get(formattedLetter) == true)
+            return false
 
         request.guessLetter(
             mapOf(
-                "token" to hangman?.token,
-                "letter" to formattedLetter
+                "token" to hangman.value?.token,
+                "letter" to formattedLetter.toString()
             )
         ).enqueue(object : Callback<HangmanModel> {
 
             override fun onResponse(call: Call<HangmanModel>, response: Response<HangmanModel>) {
-                hangman = response.body() ?: return
+                hangman.postValue(response.body() ?: return)
 
-
-                if (hangman?.correct == true) {
-                    hagmanTextOuput.text = hangman?.word?.replace("_", "_ ") ?: ""
-                } else {
-                    Toast.makeText(
-                        context,
-                        "Letter $letter is not in the word!",
-                        Toast.LENGTH_SHORT
-                    ).show()
+                alphabet.value?.apply {
+                    set(formattedLetter, true)
+                    alphabet.postValue(this)
                 }
-
-                alphabet[formattedLetter[0]] = true
-
-                var usedLetters = ""
-                alphabet.forEach { (char, isUsed) ->
-                    if (isUsed)
-                        usedLetters += "$char, "
-                }
-                alphabetTextView.text = usedLetters
             }
 
             override fun onFailure(call: Call<HangmanModel>, t: Throwable) {
-                hagmanTextOuput.text = "Error: $t"
             }
         })
-    }
 
-    fun isGameWon(): Boolean {
-        return false
-    }
-
-    fun getPunctuation(): Int {
-        return (hangman?.word?.length ?: 0) * 10 - (hangman?.incorrectGuesses ?: 0) * 5
+        return true
     }
 }
